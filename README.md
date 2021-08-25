31a99022b3fdf33d4a1043d78ec928223c3347c3
WozNet <walker.chris87@gmail.com>
2021-08-25 00:54:52 -0400

    added source and usage info

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # TaskBarPin
+pin.exe is a simple tool that allows you to programmatically pin an item to the Windows 10 Taskbar.
+
+Usage:
+Pin a Shortcut
+pin "C:\path\to\file.lnk"
+
+Unpin a Shortcut
+pin "C:\path\to\file.lnk" u
+
+Exit codes:
+-1 = printed usage
+0 = succeeded
+1 = CoInitialize failed
+2 = ILCreateFromPathW failed
+3 = CoCreateInstance failed
+4 = IPinnedList3::Modify failed
+
+
+Source: https://geelaw.blog/entries/msedge-pins/assets/pin.cc
--- pin.cc
+++ pin.cc
@@ -25,7 +25,6 @@ Source:
 https://geelaw.blog/entries/msedge-pins/assets/pin.cc
 */
 
-
 #pragma comment(lib, "ole32")
 #pragma comment(lib, "shell32")
 
@@ -34,14 +33,14 @@ https://geelaw.blog/entries/msedge-pins/assets/pin.cc
 #define UNICODE
 #define _UNICODE
 
-#include<objbase.h>
-#include<shlobj.h>
-#include<cstdio>
+#include <objbase.h>
+#include <shlobj.h>
+#include <cstdio>
 
 struct CoInitializeGuard
 {
     HRESULT const hr;
-    CoInitializeGuard() : hr(CoInitialize(NULL)) { }
+    CoInitializeGuard() : hr(CoInitialize(NULL)) {}
     ~CoInitializeGuard()
     {
         if (SUCCEEDED(hr))
@@ -55,7 +54,7 @@ struct CoInitializeGuard
 struct PIDLFromPath
 {
     PIDLIST_ABSOLUTE const pidl;
-    PIDLFromPath(PCWSTR path) : pidl(ILCreateFromPathW(path)) { }
+    PIDLFromPath(PCWSTR path) : pidl(ILCreateFromPathW(path)) {}
     ~PIDLFromPath()
     {
         if (pidl)
@@ -67,57 +66,67 @@ struct PIDLFromPath
 };
 
 const GUID CLSID_TaskbandPin =
-{
-    0x90aa3a4e, 0x1cba, 0x4233,
-    { 0xb8, 0xbb, 0x53, 0x57, 0x73, 0xd4, 0x84, 0x49}
-};
+    {
+        0x90aa3a4e, 0x1cba, 0x4233, {0xb8, 0xbb, 0x53, 0x57, 0x73, 0xd4, 0x84, 0x49}};
 
 const GUID IID_IPinnedList3 =
+    {
+        0x0dd79ae2, 0xd156, 0x45d4, {0x9e, 0xeb, 0x3b, 0x54, 0x97, 0x69, 0xe9, 0x40}};
+
+enum PLMC
 {
-    0x0dd79ae2, 0xd156, 0x45d4,
-    { 0x9e, 0xeb, 0x3b, 0x54, 0x97, 0x69, 0xe9, 0x40 }
+    PLMC_EXPLORER = 4
 };
 
-enum PLMC { PLMC_EXPLORER = 4 };
-
 struct IPinnedList3Vtbl;
-struct IPinnedList3 { IPinnedList3Vtbl *vtbl; };
+struct IPinnedList3
+{
+    IPinnedList3Vtbl *vtbl;
+};
 
 typedef ULONG STDMETHODCALLTYPE ReleaseFuncPtr(IPinnedList3 *that);
 typedef HRESULT STDMETHODCALLTYPE ModifyFuncPtr(IPinnedList3 *that,
-    PCIDLIST_ABSOLUTE unpin, PCIDLIST_ABSOLUTE pin, PLMC caller);
+                                                PCIDLIST_ABSOLUTE unpin, PCIDLIST_ABSOLUTE pin, PLMC caller);
 
 struct IPinnedList3Vtbl
 {
     void *QueryInterface;
     void *AddRef;
     ReleaseFuncPtr *Release;
-    void *MethodSlot4; void *MethodSlot5; void *MethodSlot6;
-    void *MethodSlot7; void *MethodSlot8; void *MethodSlot9;
-    void *MethodSlot10; void *MethodSlot11; void *MethodSlot12;
-    void *MethodSlot13; void *MethodSlot14; void *MethodSlot15;
+    void *MethodSlot4;
+    void *MethodSlot5;
+    void *MethodSlot6;
+    void *MethodSlot7;
+    void *MethodSlot8;
+    void *MethodSlot9;
+    void *MethodSlot10;
+    void *MethodSlot11;
+    void *MethodSlot12;
+    void *MethodSlot13;
+    void *MethodSlot14;
+    void *MethodSlot15;
     void *MethodSlot16;
     ModifyFuncPtr *Modify;
 };
 
 wchar_t const *ERR_STR_USAGE = L"Usage:\n"
-"  pin \"C:\\path\\to\\file.lnk\"      Pin a Shortcut.\n"
-"  pin \"C:\\path\\to\\file.lnk\" u    Unpin a Shortcut.\n"
-"\n"
-"Exit codes: -1 = printed usage\n"
-"             0 = succeeded\n"
-"             1 = CoInitialize failed\n"
-"             2 = ILCreateFromPathW failed\n"
-"             3 = CoCreateInstance failed\n"
-"             4 = IPinnedList3::Modify failed\n";
+                               "  pin \"C:\\path\\to\\file.lnk\"      Pin a Shortcut.\n"
+                               "  pin \"C:\\path\\to\\file.lnk\" u    Unpin a Shortcut.\n"
+                               "\n"
+                               "Exit codes: -1 = printed usage\n"
+                               "             0 = succeeded\n"
+                               "             1 = CoInitialize failed\n"
+                               "             2 = ILCreateFromPathW failed\n"
+                               "             3 = CoCreateInstance failed\n"
+                               "             4 = IPinnedList3::Modify failed\n";
 wchar_t const *ERR_STR_COINIT = L"CoInitialize failed.\n";
 wchar_t const *ERR_STR_PIDL = L"ILCreateFromPathW failed.\n";
 wchar_t const *ERR_STR_CREATE = L"CoCreateInstance failed.\n";
 wchar_t const *ERR_STR_MODIFY = L"IPinnedList3::Modify failed.\n";
 
 int const ERR_ID_USAGE = -1, ERR_ID_SUCCEEDED = 0,
-    ERR_ID_COINIT = 1, ERR_ID_PIDL = 2,
-    ERR_ID_CREATE = 3, ERR_ID_MODIFY = 4;
+          ERR_ID_COINIT = 1, ERR_ID_PIDL = 2,
+          ERR_ID_CREATE = 3, ERR_ID_MODIFY = 4;
 
 int wmain(int argc, PWSTR *argv)
 {
@@ -141,16 +150,16 @@ int wmain(int argc, PWSTR *argv)
     }
     IPinnedList3 *pinnedList;
     if (!SUCCEEDED(CoCreateInstance(
-        CLSID_TaskbandPin, NULL, CLSCTX_ALL,
-        IID_IPinnedList3, (LPVOID *)(&pinnedList))))
+            CLSID_TaskbandPin, NULL, CLSCTX_ALL,
+            IID_IPinnedList3, (LPVOID *)(&pinnedList))))
     {
         fputws(ERR_STR_CREATE, stderr);
         return ERR_ID_CREATE;
     }
     HRESULT hr = pinnedList->vtbl->Modify(pinnedList,
-        pinning ? NULL : pidl,
-        pinning ? pidl : NULL,
-        PLMC_EXPLORER);
+                                          pinning ? NULL : pidl,
+                                          pinning ? pidl : NULL,
+                                          PLMC_EXPLORER);
     pinnedList->vtbl->Release(pinnedList);
     if (!SUCCEEDED(hr))
     {
